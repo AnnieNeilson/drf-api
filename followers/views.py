806c8ef02b06a55d71cc9d7daf6c2ecfc7df19a9
AnnieNeilson <bnefnee@gmail.com>
2022-10-04 08:58:17 +0000

    Add JSON Web Tokens to the project and configured them

--- followers/views.py
+++ followers/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from followers.models import Follower
 from followers.serializers import FollowerSerializer
@@ -7,7 +8,12 @@ class FollowerList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = FollowerSerializer
     queryset = Follower.objects.all()
-
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+    ]
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
